f80515a33c07b87411e139f9bd67cdd7bb764d35
JanoVega <a.vega06@ufromail.cl>
2023-01-11 18:06:14 -0300

    check all/no print

--- main_scraper.py
+++ main_scraper.py
@@ -64,7 +64,6 @@ class Crawler():
                 except AssertionError as e:
                     informe_row += '    '+site +': '+ str(e) +'\n'
                 except Exception as e:
-                    print(e)
                     informe_row += '    '+site +': '+ str(e) +'\n'
                 time.sleep(5)
                     
