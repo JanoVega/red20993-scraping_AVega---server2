9181f6107871baed9e1546b974a0fd1a3b8d1cb2
JanoVega <a.vega06@ufromail.cl>
2023-01-11 17:51:19 -0300

    print path on trabajando

--- scrapers/trabajando.py
+++ scrapers/trabajando.py
@@ -19,8 +19,8 @@ from utils.csv_utils import load_date, load_url, save_to_csv, save_urls
 from utils.csv_utils import save_to_failed_links_csv
 
 # ubicacion del ejecutable para chromedriver 
-# ! puede ser que sólo funcione con chromedriver
 path = os.getcwd() + '/chromedriver'
+print(path)
 service = Service(executable_path=path)
 
 def get_to_page(driver, url, search_keyword):
