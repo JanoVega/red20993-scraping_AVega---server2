f91dc5c2443c0768069af5c3b3b6ce0fb16c9277
JanoVega <a.vega06@ufromail.cl>
2023-01-16 00:11:17 -0300

    date

--- script.py
+++ script.py
@@ -38,7 +38,7 @@ sites = ['chiletrabajos',\
 
     
 """ formato  día/mes/año, dia y mes sin 0 a la izquerda, ej: 2/5/2022 """
-dia_primera_ejecucion = '15/1/2023'
+dia_primera_ejecucion = '16/1/2023'
 
 if get_date('hoy') == dia_primera_ejecucion:
     """ para instanciar los csv """
